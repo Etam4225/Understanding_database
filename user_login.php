3e87e605773e6effeac98f1b854110335842c94e
asura124 <44069065+asura124@users.noreply.github.com>
2021-04-29 15:20:44 -0400

    Updated: added state,city, street and payment

--- user_login.php
+++ user_login.php
@@ -3,11 +3,19 @@
   <input type ="varchar(16)" name="name" placeholder="UserName">
   <br>
   <input type ="varchar(64)" name="pass" placeholder="Password">
+  <br>
+  <input type ="varchar(2)" name="state" placeholder="State">
+  <br>
+  <input type ="varchar(64)" name="city" placeholder="City">
+  <br>
+  <input type ="varchar(64)" name="street" placeholder="Street">
+  <br>
+  <input type ="varchar(16)" name="payment" placeholder="Payment method">
   <button type="submit" name="submit">
     Sign up
   </button>
 </form>
-
+  
 <form action= "interface.php" method=POST>
   <input type ="varchar(16)" name="login_name" placeholder="UserName">
   <br>
