15bbcb0d64cf7261ada4f507dea691d04a440f5c
etam4225 <etam4225@bths.edu>
2021-05-09 11:56:04 -0400

    Update interface.php

--- interface.php
+++ interface.php
@@ -17,7 +17,42 @@ session_start();
 			<nav>
 			  <ul class = "navigation"> <!-- Placeholder header to use on other pages -->
 				  <li><a href="#"> About us </a></li>
-				  <li><a href="#"> PLACEHOLDER </a></li>
+				  <li>
+            <?php
+                include "database_login_info.php";
+
+                $mysqli->close();
+                
+                
+                $word ='';
+                $passw ='';
+                $state ='';
+                $city ='';
+                $street ='';
+                $payment ='';
+                $login_word='';
+                $login_passw='';
+                
+                $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+                //Sets the UserNames/Passwords
+                if(isset($_POST['login_submit'])){
+                  $login_word = $_POST["login_name"];
+                  $login_passw = $_POST["login_pass"];
+                  $_SESSION["username"] = $login_word;
+                }
+                else if(isset($_POST['submit'])){
+                  $word = $_POST["name"];
+                  $passw = $_POST["pass"];
+                  $state = $_POST["state"];
+                  $city = $_POST["city"];
+                  $street = $_POST["street"];
+                  $payment = $_POST["payment"];
+                }
+              echo "<br>" ;
+              $link_address = "../cart.php?rn=$login_word";
+              echo "<td>" . '<a href="'.$link_address.'"> MY CART </a>' . "</td>"; 
+            ?>
+          </li>
           <li>  
             <form method=POST> <!-- sign out button -->
               <button type="submit" name="back">
@@ -31,36 +66,6 @@ session_start();
 </div>
 
 <?php
-include "database_login_info.php";
-
-$mysqli->close();
-
-
-$word ='';
-$passw ='';
-$state ='';
-$city ='';
-$street ='';
-$payment ='';
-$login_word='';
-$login_passw='';
-
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
-
-//Sets the UserNames/Passwords
-if(isset($_POST['login_submit'])){
-  $login_word = $_POST["login_name"];
-  $login_passw = $_POST["login_pass"];
-  $_SESSION["username"] = $login_word;
-}
-else if(isset($_POST['submit'])){
-  $word = $_POST["name"];
-  $passw = $_POST["pass"];
-  $state = $_POST["state"];
-  $city = $_POST["city"];
-  $street = $_POST["street"];
-  $payment = $_POST["payment"];
-}
 
 //checks if the User is login or Signing up
 if($word !='' && $passw !='' && $state !='' && $city !='' && $street !='' && $payment !='' && $login_word=='' && $login_passw==''){
@@ -98,25 +103,17 @@ if(isset($_POST['back']))
 
 <br>
 
-<form action = "keyword_search.php" >
-   Enter the game you are looking for: <br>
-  <input type="text" name="keyword"><br>
-  <select name="Sorting">
-    <option selected disabled> SELECT </option>
-    <option value="name"> name </option>
-    <option value="Store"> Store </option>
-    <option value="copy"> Copy </option>
-    <option value="Console"> Console </option>
-    <option value="rating"> Rating </option>
-    <option value="price"> Price </option>
-  </select>
-  <input type="submit" value="Search!">
-</form>
-
-
-<?php
-echo "<br>" ;
-$link_address = "../cart.php?rn=$login_word";
-echo "<td>" . '<a href="'.$link_address.'"> MY CART </a>' . "</td>"; 
-  
-?>
+  <form action = "keyword_search.php" >
+    Enter the game you are looking for: <br>
+    <input type="text" name="keyword"><br>
+    <select name="Sorting">
+      <option selected disabled> SELECT </option>
+      <option value="name"> name </option>
+      <option value="Store"> Store </option>
+      <option value="copy"> Copy </option>
+      <option value="Console"> Console </option>
+      <option value="rating"> Rating </option>
+      <option value="price"> Price </option>
+    </select>
+    <input type="submit" value="Search!">
+  </form>
