ccbc170e90121c7148d550526be93765609b1b2f
asura124 <44069065+asura124@users.noreply.github.com>
2021-05-10 20:25:53 -0400

    Added some Comments

--- remove.php
+++ remove.php
@@ -12,14 +12,13 @@ session_start()
 <div class = "header">  
 		<div class = "inner_header">
 			<div class = "logo_container">
-				<img src = "images/logo.png" class = "logo" id = "logo_img"> <!-- clicking on this does nothing currently. -->
+				<img src = "images/logo.png" class = "logo" id = "logo_img"> 
 			</div>
 
       <nav>
-			<ul class = "navigation"> <!-- Placeholder header to use on other pages -->
-				<li><a href="#"> About us </a></li>
+			<ul class = "navigation"> 
 				<li><form method=POST><p><button type="submit" name="back">Back</button></p></form></li>
-        		<li><form method=POST><p><button type="submit" name="out">Checkout</button></p></form></li>
+        <li><form method=POST><p><button type="submit" name="out">Checkout</button></p></form></li>
 			</ul>
 			</nav>
     </div>
@@ -28,12 +27,12 @@ session_start()
 
 <?php
 include "database_login_info.php";
-$rollno = $_GET['rn'];
-$username = $_SESSION["username"];
+$rollno = $_GET['rn']; // get what the user wants to remove from their cart
+$username = $_SESSION["username"]; //get current user's username
 
 echo "<p><center><h2> Hello, " . $username . "<br>" . "</h2></center></p>";
 echo "<p><center><h2> #" . $rollno . " has been removed from your cart. </h2></center></p>";
-
+//removes game from their cart
 $sql = "DELETE FROM Cart where username ='" . $username . "' AND gameID ='" . $rollno . "';";
 $result = $mysqli->query($sql);
 
